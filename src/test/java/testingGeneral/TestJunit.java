06c5e39f2cda89f344962ea599e22abc3edc3332
saltytcodes <43404828+SaltLithe@users.noreply.github.com>
2021-02-09 21:45:04 +0100

    Added test base

---
+++ src/test/java/testingGeneral/TestJunit.java
@@ -0,0 +1,12 @@
+package testingGeneral;
+import org.junit.Test;
+
+public class TestJunit {
+	
+   
+
+   @Test
+   public void testPrintMessage() {
+	   System.out.println("Hello jUnit");
+   }
+}
